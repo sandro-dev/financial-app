c25fdf00e343c44d07d8ba34454a9f364e5fddb1
Sandro Santos <sandro.dev@gmail.com>
2022-03-27 18:34:20 -0300

    chore: summary load values dinamically

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,10 +1,39 @@
 import { Box, Container, Content } from "./styles";
 
+import { useTransactions } from "../../hooks/useTransactions";
+
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
 import totalImg from '../../assets/total.svg';
 
 export const Summary: React.FC = () => {
+
+  const { transactionsData } = useTransactions();
+
+  //const totalIncome = transactionsData.reduce((acc, transaction) => transaction.type === 'credit' ? acc += transaction.price : acc, 0)
+  const totalIncome = transactionsData.reduce((acc, transaction) => {
+    if(transaction.type === 'credit') {
+      return acc += transaction.price;
+    }
+
+    return acc;
+  }, 0);
+
+ // const totalOutcome = transactionsData.reduce((acc, transaction) => transaction.type === 'debit' ? acc -= transaction.price : acc, 0);
+  const totalOutcome = transactionsData.reduce((acc, transaction) => {
+    if(transaction.type === 'debit') {
+      return acc -= transaction.price;
+    }
+
+    return acc;
+  }, 0);
+
+  const total = totalIncome - totalOutcome;
+  
+  console.log('total income => ', totalIncome)
+  console.log('total outcome => ', totalOutcome)
+  console.log('total => ', total);
+
   return (
     <Container>
       <Content>
@@ -13,21 +42,35 @@ export const Summary: React.FC = () => {
             <span>Entradas</span>
             <img src={incomeImg} alt="entradas" />
           </header>          
-            <strong>R$ 15.500,00</strong>
+            <strong>
+              {
+                new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(totalIncome)
+              }
+            </strong>
         </Box>
         <Box>
           <header>
             <span>Saídas</span>
             <img src={outcomeImg} alt="saídas" />
           </header>          
-            <strong>R$ 4.360,00</strong>
+            <strong>
+              {
+                new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL'})
+                        .format(totalOutcome)
+              }
+            </strong>
         </Box>
         <Box>
           <header>
             <span>Total</span>
             <img src={totalImg} alt="total" />
           </header>          
-            <strong>R$ 11.140,00</strong>
+            <strong>
+              {
+                new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL'})
+                        .format(total)
+              }
+            </strong>
         </Box>
       </Content>
     </Container>
